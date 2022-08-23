12366820a6d2b7d8e2dde5c78329524bcc9fc955
Shenmirenwu3112 <lauejie@gmail.com>
2022-08-23 14:30:25 +0800

    update

--- LCS/chatwindow.cpp
+++ LCS/chatwindow.cpp
@@ -1,9 +1,6 @@
 #include "chatwindow.h"
 #include "ui_chatwindow.h"
 
-#include "emoticonwidget.h"
-#include "emojipack.h"
-
 chatWindow::chatWindow(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::chatWindow)
@@ -17,7 +14,66 @@ chatWindow::chatWindow(QWidget *parent) :
     ui->labelChatting->setAttribute(Qt::WA_TranslucentBackground,true);
     ui->labelChatting->setMargin(10);
 
-    
+    ui->labelFileStatus->setText("");
+
+    isStart= true;
+
+//    connect(tcpSocket,&QTcpSocket::connected,[=](){
+//       // show connection status bottom right
+//        ui->labelConnectionStatus->setText("Connected");
+//    });
+//    connect(tcpSocket,&QTcpSocket::readyRead,[=](){
+//       QByteArray buf = tcpSocket->readAll();
+
+//       if(isStart){
+//           isStart = false;
+
+//           fileName = QString(buf).section("##",0,0);
+//           fileSize = QString(buf).section("##", 1, 1).toInt();
+//           recvSize = 0;
+//           //打开文件
+//           //关联文件名字
+//           file.setFileName(fileName);
+
+//           //只写方式打开文件
+//           bool isOk = file.open(QIODevice::WriteOnly);
+//           if(false == isOk)
+//           {
+//               qDebug() << "WriteOnly error 49";
+
+//               tcpSocket->disconnectFromHost(); //断开连接
+//               tcpSocket->close(); //关闭套接字
+
+//               return; //如果打开文件失败，中断函数
+//           }
+//           QString str = QString("接收的文件: [%1: %2kb]").arg(fileName).arg(fileSize/1024);
+//           QMessageBox::information(this, "文件信息", str);
+//           ui->labelFileStatus->setText(str);
+//           ui->labelFileStatus->setText("正在接收文件……");
+
+//       }
+//       else{
+//           qint64 len = file.write(buf);
+//           if(len >0) //接收数据大于0
+//           {
+//               recvSize += len; //累计接收大小
+//               qDebug() << len;
+//           }
+
+//           if(recvSize == fileSize) //文件接收完毕
+//           {
+//               //先给服务发送(接收文件完成的信息)
+//               tcpSocket->write("Done.");
+
+//               ui->textEdit->append("文件接收完成");
+//               QMessageBox::information(this, "完成", "文件接收完成");
+//               file.close(); //关闭文件
+//               //断开连接
+//               tcpSocket->disconnectFromHost();
+//               tcpSocket->close();
+//            }
+//       }
+//    });
 }
 
 chatWindow::~chatWindow()
@@ -27,7 +83,7 @@ chatWindow::~chatWindow()
 
 void chatWindow::on_pushButtonFile_clicked()
 {
-    QString filePath = QFileDialog::getOpenFileName(this,"Open file","D:/");
+    QString filePath = QFileDialog::getOpenFileName(this,"Open file","D:/",tr("All files(*.*)"));
     fPath = filePath;
     if(false == filePath.isEmpty()) //如果选择文件路径有效
     {
@@ -55,15 +111,14 @@ void chatWindow::on_pushButtonFile_clicked()
         //提示打开文件的路径
         ui->textEdit->append(filePath);
     }
-    else
-    {
+    else{
         qDebug() << "选择文件路径出错 118";
     }
 }
 
 void chatWindow::on_pushButtonPic_clicked()
 {
-    QString filePath = QFileDialog::getOpenFileName(this,"Open file","D:/");
+    QString filePath = QFileDialog::getOpenFileName(this,"Open file","D:/",tr("JPEG(*.jpg *.jpeg);; PNG(*.png);; MP3(*.mp3);; MP4(.mp4)"));
     fPath = filePath;
     if(false == filePath.isEmpty()) //如果选择文件路径有效
     {
@@ -97,15 +152,10 @@ void chatWindow::on_pushButtonPic_clicked()
     }
 }
 
-void chatWindow::on_pushButtonEmoji_clicked()
-{
-
-}
-
 void chatWindow::on_pushButtonSend_clicked()
 {
     //先发送文件头信息  文件名##文件大小
-//    QString head = QString("%1##%2").arg(fileName).arg(fileSize);
+    QString head = QString("%1##%2").arg(fileName).arg(fileSize);
     //发送头部信息
 //    qint64 len = tcpSocket->write( head.toUtf8() );
 //    if(len > 0)//头部信息发送成功
@@ -122,9 +172,9 @@ void chatWindow::on_pushButtonSend_clicked()
 //        file.close();
 //    }
 
-    QImage p;
-    p.load(":/image/defaultuser.jpg");
-    p.scaled(10,10,Qt::KeepAspectRatioByExpanding,Qt::SmoothTransformation);
+    //QImage p;
+    //p.load(":/image/defaultuser.jpg");
+    //p.scaled(10,10,Qt::KeepAspectRatioByExpanding,Qt::SmoothTransformation);
 
 
     chatStr = ui->textEdit->toPlainText();
@@ -160,4 +210,17 @@ bool chatWindow::loadThemeFile(QString str){
     lobConfigFile->close();
 
     return true;
+}
+
+void chatWindow::on_pushButtonConnect_clicked()
+{
+//    //获取服务器的ip和端口
+//    QString ip = ui->lineEditIp->text();
+//    quint16 port = ui->lineEditPort->text().toInt();
+
+//    //主动和服务器连接
+//    tcpSocket->connectToHost(QHostAddress(ip), port);
+
+//    isStart = true;
+
 }
